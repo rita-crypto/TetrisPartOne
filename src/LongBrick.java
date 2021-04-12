1b9941c1747d3e04a48cfcd562fd25e47a311ef0
rita-crypto <rita.khadka552@gmail.com>
2021-04-12 14:32:46 -0500

    created bricks

---
+++ src/LongBrick.java
@@ -0,0 +1,14 @@
+/*
+ * The purpose of this program is to implement the game board for the tetris with a series of
+          pieces spawning over the game board.
+ * @author prawjaldhungana, ritakhadka
+ * April 12, 2021
+
+ */
+
+
+import javax.swing.*;
+
+
+public class LongBrick {
+}
